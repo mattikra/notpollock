e874e2cedab60bee2c609f104df940a5acb4a928
Matthias Krauss <mattikra@gmail.com>
2016-08-15 11:11:48 +0200

    Parameter adjustments

--- CamCapture/Settings.h
+++ CamCapture/Settings.h
@@ -14,15 +14,15 @@
 #define BEHAVIOUR_CLASS SimplePollockBehaviour
 #define BEHAVIOUR_HEADER "SimplePollockBehaviour.h"
 
-#define TEMPLATE_NAME @"pollock1"
+#define TEMPLATE_NAME @"Slice2"
 
 //tracking parameters
-#define THRESHOLD_PERCENT 90 /* threshold with x percent of brightest pixel */
-#define WINDOW_MIN 10         /* minimum valid BBOX size */
-#define WINDOW_MAX 50        /* maximum valid BBOX size */
+#define THRESHOLD_PERCENT 61.6 /* threshold with x percent of brightest pixel */
+#define WINDOW_MIN 10          /* minimum valid BBOX size */
+#define WINDOW_MAX 50          /* maximum valid BBOX size */
 #define SHOW_ROI YES
 #define SHOW_THRES YES
-#define ROI_SIZE 0.533
+#define ROI_SIZE 0.439
 
 //path estimation properties
 #define MIN_TRACK_SEQUENCE 20
@@ -32,9 +32,9 @@
 //latencies / physical properties
 #define PENDULUM_LENGTH 4.81  /* length of pendulum in m */
 #define CAN_HEIGHT 0.07       /* can height above canvas in m */
-#define CANVAS_SIZE_M 1.5     /* canvas size (one edge of square) in m */
+#define CANVAS_SIZE_M 2.72    /* canvas size (one edge of square) in m */
 #define VALVE_LATENCY 0.185   /* valve release latency in s (e.g. BLE / physical) */
-#define VALVE_ON_TIME_S 0.1   /* valve on time in s */
+#define VALVE_ON_TIME_S 0.066 /* valve on time in s */
 #define DROP_DEAD_TIME_S 0.5  /* dead time after each drop in s */
 
 /* DITHERING */
